c62c34d779cf137e718b11827f0cafa0c79094f8
gabriellespits <gabrielle-spits@hotmail.com>
2024-01-20 15:26:10 +0100

    doe je iets

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,30 +20,34 @@
           <a class="router-link" @click="toggleDropdown">Profiel</a>
           <div v-show="dropdownOpen" class="dropdown-content">
             <!-- Dropdown items hier -->
-            <li class="dropdown-li"><p>email: {{ username }}</p></li>
-            <li class="dropdown-li"><p>rol: {{ role }}</p></li>
-            <li class="dropdown-li"><a class="uitloggen" @click="logoutAuth()">Uitloggen</a></li>
-          </div>
-        </div>
+      <li class="dropdown-li">
+        <p>email: {{ username }}</p>
       </li>
+      <li class="dropdown-li">
+        <p>rol: {{ role }}</p>
+      </li>
+      <li class="dropdown-li"><a class="uitloggen" @click="logoutAuth()">Uitloggen</a></li>
+  </div>
+  </div>
+  </li>
 
-      <li v-if="role === 'admin'" class="admin">
-        <router-link class="router-link" to="/GebruikerLijst">Gebruikers</router-link>
-      </li>
-      <li v-if="isAuthenticated === true" class="admin">
-        <router-link class="router-link" to='/Location'>Locatie</router-link>
-      </li>
-      <li v-if="isAuthenticated === true" class="admin">
-        <router-link class="router-link" to='/ArduinoLocation'>Arduino</router-link>
-      </li>
-      <li v-if="isAuthenticated === true"  class="admin">
-        <router-link class="router-link" to='/Loggen'>Logs</router-link>
-      </li>
-      <!-- Alleen weergeven als gebruiker is ingelogd als guest of admin -->
-      <li v-if="role === 'guest' || role === 'admin' || role === 'manager'" class="login">
-        <a class="router-link" @click="handleLogout">Uitloggen</a>
-      </li>
-    </nav>
+  <li v-if="role === 'admin'" class="admin">
+    <router-link class="router-link" to="/GebruikerLijst">Gebruikers</router-link>
+  </li>
+  <li v-if="isAuthenticated === true" class="admin">
+    <router-link class="router-link" to='/Location'>Locatie</router-link>
+  </li>
+  <li v-if="isAuthenticated === true" class="admin">
+    <router-link class="router-link" to='/ArduinoLocation'>Arduino</router-link>
+  </li>
+  <li v-if="isAuthenticated === true" class="admin">
+    <router-link class="router-link" to='/Loggen'>Logs</router-link>
+  </li>
+  <!-- Alleen weergeven als gebruiker is ingelogd als guest of admin -->
+  <li v-if="role === 'guest' || role === 'admin' || role === 'manager'" class="login">
+    <a class="router-link" @click="handleLogout">Uitloggen</a>
+  </li>
+  </nav>
   </div>
 </template>
 
@@ -62,7 +66,7 @@ export default {
       isAuthenticated: false, // Aangeven of de gebruiker toegang heeft
       isLoading: null, // Meegeven of de oAuth data al is opgehaald
       dropdownOpen: false,
-      test:1,
+      test: 1,
     };
   },
   methods: {
@@ -81,7 +85,7 @@ export default {
       if (this.isAuthenticated) {
         console.log(this.currentUser.email);
         sessionStorage.setItem("username", this.currentUser.email);
-        this.username =this.currentUser.email;
+        this.username = this.currentUser.email;
         sessionStorage.setItem("inlogmoment", new Date().toLocaleString("nl-NL").replace('T', ' ').replace('Z', ''));
 
         try {
@@ -93,7 +97,7 @@ export default {
         }
         // Haalt de rol van de ingelogde gebruiker op
         this.getUserRoleAuth();
-      } else if(this.role == 'admin'){
+      } else if (this.role == 'admin') {
 
       }
       // De gebruiker is niet ingelogd. De session storage leegmaken
@@ -163,12 +167,20 @@ export default {
       // Leeg, want hij wordt toch overschreven in de mounted()
     },
     logoutAuth() {
-      this.$auth0.logout({ logoutParams: { returnTo: 'https://wonderful-stone-0a33b7110.4.azurestaticapps.net/' } })
+      this.$auth0.logout({
+        logoutParams: {
+          returnTo: 'https://wonderful-stone-0a33b7110.4.azurestaticapps.net/'
+        }
+      }).then(() => {
+        console.log('Logout successful');
+      }).catch(error => {
+        console.error('Logout error:', error);
+      });
     },
     handleLogout() {
       // hier haal ik email op
       const email = sessionStorage.getItem('username');
-      
+
       // hier maak ik sessie leeg
       sessionStorage.clear();
       window.location.reload();
@@ -258,7 +270,7 @@ li {
   text-decoration: none;
 }
 
-.uitloggen{
+.uitloggen {
   display: block;
   color: white;
   text-align: center;
@@ -273,17 +285,18 @@ li {
 
 a {
   cursor: pointer;
- 
+
   /* Voeg hier eventueel andere stijlen toe */
 }
 
-a:hover{
+a:hover {
   text-decoration: underline;
 }
 
-.dropdown-content{
+.dropdown-content {
   border-top: 1px solid #bbb;
 }
+
 .dropdown-content .dropdown-li {
   border-top: 1px solid #bbb;
 
@@ -291,7 +304,4 @@ a:hover{
   display: block;
   text-align: center;
   color: white;
-}
-
-
-</style>
+}</style>
